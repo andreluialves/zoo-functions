bc632e524514ad67b5d2bba9ee846faef2997b15
André Alves <andreluialves@hotmail.com>
2022-01-25 18:43:37 -0300

    Requisito 1 cumprido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui 
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return []
+  } else {
+    const { species } = data;
+    const findIds = (element) => {
+      if (ids.includes(element.id) === true) {
+        return element.id;
+      }
+    };
+    return species.filter(findIds);
+  }
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;

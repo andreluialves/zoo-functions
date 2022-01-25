f253cd683e58ddd1b04b46622e670e44accd2278
André Alves <andreluialves@hotmail.com>
2022-01-25 18:51:37 -0300

    Corrigido erro de lint.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,16 +3,14 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   if (ids === undefined) {
-    return []
-  } else {
-    const { species } = data;
-    const findIds = (element) => {
-      if (ids.includes(element.id) === true) {
-        return element.id;
-      }
-    };
-    return species.filter(findIds);
+    return [];
   }
+  const findIds = (element) => {
+    if (ids.includes(element.id) === true) {
+      return element.id;
+    }
+  };
+  return species.filter(findIds);
 }
 
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));

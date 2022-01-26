08fd1e11c1a8886fca1978cbef61256c48acbea6
André Alves <andreluialves@hotmail.com>
2022-01-25 21:17:05 -0300

    Requisito 2 cumprido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const verifyName = (element) => element.name === animal;
+  const verifyAge = (element) => element.age >= age;
+  const testFindAnimal = species.find(verifyName);
+  const { residents } = testFindAnimal;
+  return residents.every(verifyAge);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;

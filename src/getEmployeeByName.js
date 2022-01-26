39eedb1a3cd2434254eac785af453d869b76f706
André Alves <andreluialves@hotmail.com>
2022-01-25 23:26:45 -0300

    Requisito 3 cumprido.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const { employees } = data;
+  const saveEmployee = employees.find(
+    (element) => element.firstName === employeeName || element.lastName === employeeName,
+  );
+  return saveEmployee;
 }
 
 module.exports = getEmployeeByName;

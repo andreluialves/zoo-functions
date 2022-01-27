5a0bd24814d5f3b8f62b12870faf607b44ffe3f3
André Alves <andreluialves@hotmail.com>
2022-01-26 21:03:48 -0300

    Requisito 5 cumprido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  if (!animal) {
+    const result = {};
+    const callback = (element) => { result[element.name] = element.residents.length; };// Callback
+    species.map(callback);
+    return result;
+  }
+  if (animal.specie && !animal.sex) {
+    const callback = (element) => element.name === animal.specie; // Callback
+    const animalSpecieArr = species.filter(callback);
+    const { residents } = animalSpecieArr[0];
+    return residents.length;
+  }
+  const callback = (element) => element.name === animal.specie; // Callback
+  const animalSpecieAndSex = species.filter(callback);
+  const { residents } = animalSpecieAndSex[0];
+  const count = residents.filter((element) => element.sex === animal.sex);
+  return count.length;
 }
 
 module.exports = countAnimals;

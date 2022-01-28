a01cd049f5e445b0da43aa19b67d57d4e4d5f69c
André Alves <andreluialves@hotmail.com>
2022-01-28 11:09:55 -0300

    Requisito 9 cumprido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,54 @@
 const data = require('../data/zoo_data');
+const { species } = data;
+
+const callBackByLocality = (acc, curr) => { // Callback
+  const { name, location, residents } = curr;
+  if (!acc[location]) {
+    acc[location] = [];
+  }
+  acc[location].push(name);
+  return acc;
+};
+
+const namesByLocality = () => species.reduce(callBackByLocality, {}); // Return names by locality.
+
+const callBackNamesResidents = (acc, curr) => { // Callback
+  const {name, location, residents } = curr;
+  const { sex } = residents
+  if (!acc[location]) {
+    acc[location] = [];
+  }
+  acc[location].push({[name]: residents.map((resident) => resident.name)});
+  return acc;
+};
+
+const byNames = (sorted, sex) => species.reduce( callBackNamesResidents, {}); // Return names and residents.
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || !options.includeNames) { return namesByLocality(); }
+  // if (options.includeNames && options.sorted && options.sex) { return byNames(); }
+  if (options.includeNames) { return byNames(); }
+  // if (options.sorted === true && !options.sorted && options.includeNames) { return byNames(); }
+
+
+  return namesByLocality();
 }
+
+const expected = {
+  NE: ['lions', 'giraffes'],
+  NW: ['tigers', 'bears', 'elephants'],
+  SE: ['penguins', 'otters'],
+  SW: ['frogs', 'snakes'],
+};
+
+// const options = { sex: 'female', sorted: true };
+// const options = { includeNames: true };
+const options = { includeNames: true, sorted: true };
+
+
+// console.log(getAnimalMap(expected));
+console.log(getAnimalMap(options));
+
+
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { species } = data;
+  const { employees } = data;
+  const findEmployee = employees.find((element) => element.id === id);
+  const findSpecie = species.find((element) => element.id === findEmployee.responsibleFor[0]);
+  const findOldAge = findSpecie.residents.reduce((acc, curr) => {
+    if (acc.age > curr.age) { return acc; }
+    return curr;
+  });
+  const result = Object.values(findOldAge);
+  return result;
 }
 
 module.exports = getOldestFromFirstSpecies;
